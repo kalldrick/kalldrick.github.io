c1132e2ae974fa0cc5bdaa0f201a0024b9dd92b4
Kalldrick <kaldri170@gmail.com>
2022-09-03 16:43:16 +0200

    Last Commited

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@ function slide(){
     document.getElementById("my-img").style.clipPath="polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
 }
 
+function display(){
+    const header = document.getElementById('header');
+    let hamburger = document.getElementById('navbar');
+    if(hamburger.style.display === "block"){
+        hamburger.style.display = "none";
+        header.style.background = "none"
+    }
+    else{
+        hamburger.style.display = "block";
+        header.style.background = "#E3E2DF";
+    }
+}
+
